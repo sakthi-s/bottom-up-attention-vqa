98bb55afb6726969eb16081f42a2962373381250
Sakthivel Sivaraman <sakthi2793@gmail.com>
2018-04-17 04:09:13 -0400

    dimension mismatch

--- attention.py
+++ attention.py
@@ -76,8 +76,9 @@ class VGGAttention(nn.Module):
         return w
 
     def logits(self, v, q, batch, k):
-        q_proj = self.q_proj(q).unsqueeze(2).repeat(1, k, k, 1)
-        joint_repr = v_proj * q_proj
+	import pdb; pdb.set_trace()
+        q_proj = self.q_proj(q).unsqueeze(1).repeat(1, k, k, 1)
+        joint_repr = v * q_proj
         joint_repr = self.dropout(joint_repr)
         logits = self.conv(joint_repr)
         logits = logits.view(batch, k*k, 1)
